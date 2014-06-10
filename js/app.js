31b820ea7e40c29137704af3928d2498f03923ed
Ben Centra <blcentra@gmail.com>
2014-06-09 23:40:15 -0400

    working on thunderdome settings

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@ app.config(['$routeProvider', function($routeProvider) {
 			templateUrl: 'partials/drops.html',
 			controller: 'DropCtrl'
 		}).
-		when('/api', {
-			templateUrl: 'partials/api.html',
-			controller: 'APICtrl'
+		when('/settings', {
+			templateUrl: 'partials/settings.html',
+			controller: 'SettingsCtrl'
 		}).
 		/*when('/thunderdome', {
 			templateUrl: 'partials/thunderdome.html',
@@ -122,25 +122,50 @@ app.factory("DropService", function($http, $window, $log) {
 });
 
 // API Service - generate or retrieve a user's API key
-app.factory("APIService", function($http, $window, $log) {
+app.factory("SettingsService", function($http, $window, $log) {
 	return {
-		// Get a user's API key
+		// Get your API key
 		retrieveKey: function(successCallback, errorCallback) {
 			$http({
 				method: "GET",
 				url: baseUrl+"users/apikey"
 			}).success(successCallback).error(errorCallback);
 		},
+		// Generate a new API key
 		generateKey: function(successCallback, errorCallback) {
 			$http({
 				method: "POST",
 				url: baseUrl+"users/apikey"
 			}).success(successCallback).error(errorCallback);
 		},
+		// Delete your API key
 		deleteKey: function(successCallback, errorCallback) {
 			$http({
 				method: "DELETE",
 				url: baseUrl+"users/apikey"
+			}).success(successCallback).error(errorCallback);
+		},
+		// Get your thunderdome settings
+		getThunderdome: function(successCallback, errorCallback) {
+			$http({
+				method: "GET",
+				url: baseUrl+"users/thunderdome"
+			}).success(successCallback).error(errorCallback);
+		},
+		// Update your thunderdome settings
+		updateThunderdome: function(twitter, quietHours, enabled, successCallback, errorCallback) {
+			$http({
+				method: "POST",
+				url: baseUrl+"users/thunderdome",
+				data: jQuery.param({"twitter":twitter, "quiet_hours":quietHours, "enabled":enabled}),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).success(successCallback).error(errorCallback);
+		},
+		// Delete your thunderdome settings
+		deleteThunderdome: function(successCallback, errorCallback) {
+			$http({
+				method: "DELETE",
+				url: baseUrl+"users/thunderdome"
 			}).success(successCallback).error(errorCallback);
 		}
 	};
@@ -663,15 +688,17 @@ function DropCtrl($scope, $window, $log, DropService) {
 }
 
 // Controller for the API page
-function APICtrl($scope, $window, $log, APIService) {
+function SettingsCtrl($scope, $window, $log, SettingsService) {
 	$scope.api_key = false;	// User's API key
 	$scope.date = "";		// Date the API key was generated
-	// Message to display to the user
-	$scope.message = "Looks like you need an API key!";
+	$scope.api_message = "Looks like you need an API key!";
+	$scope.twitter = "";
+	$scope.quiet_hours = [];
+	$scope.enabled = true;
 
 	// Get the user's API key
 	$scope.retrieveKey = function() {
-		APIService.retrieveKey(
+		SettingsService.retrieveKey(
 			function (response) {
 				if (response.status) {
 					$scope.api_key = response.data.api_key;
@@ -679,7 +706,7 @@ function APICtrl($scope, $window, $log, APIService) {
 				}
 				else {
 					$scope.api_key = false;
-					$scope.message = "Looks like you need an API key!";
+					$scope.api_message = "Looks like you need an API key!";
 					$log.log(response.message);
 				}
 			},
@@ -692,8 +719,8 @@ function APICtrl($scope, $window, $log, APIService) {
 	// Generate a new key for the user
 	$scope.generateKey = function() {
 		$scope.api_key = false;
-		$scope.message = "Generating your API key...";
-		APIService.generateKey(
+		$scope.api_message = "Generating your API key...";
+		SettingsService.generateKey(
 			function (response) {
 				if (response.status) {
 					$scope.api_key = response.data.api_key;
@@ -711,11 +738,11 @@ function APICtrl($scope, $window, $log, APIService) {
 
 	// Delete the user's current API key
 	$scope.deleteKey = function () {
-		APIService.deleteKey(
+		SettingsService.deleteKey(
 			function (response) {
 				if (response.status) {
 					$scope.api_key = false;
-					$scope.message = "Looks like you need an API key!";
+					$scope.api_message = "Looks like you need an API key!";
 					$scope.date = "";
 				}
 				else {
@@ -728,8 +755,90 @@ function APICtrl($scope, $window, $log, APIService) {
 		);
 	}
 
+	// Add another quiet hours
+	$scope.addQuietHours = function () {
+		$scope.quiet_hours.push({"id":$scope.quiet_hours.length+1, "start":"00:00", "end":"00:00"});
+		console.log($scope.quiet_hours);
+	}
+
+	// Remove a quiet hours
+	$scope.removeQuietHours = function (hours) {
+		for (var i = 0; i < $scope.quiet_hours.length; i++) {
+			if ($scope.quiet_hours[i].id == hours.id) {
+				$scope.quiet_hours.splice(i, 1);
+				break;
+			}
+		}
+	}
+
+	// Enable thunderdome
+	$scope.toggleThunderdome = function () {
+		$scope.enabled = !$scope.enabled;
+	}
+
+	// Get your Thunderdome settings
+	$scope.getThunderdome = function () {
+		SettingsService.getThunderdome(
+			function (response) {
+				if (response.status) {
+					$scope.twitter = response.data.twitter;
+					$scope.quiet_hours = JSON.parse(response.data.quiet_hours);
+					$scope.enabled = response.data.enabled == 1 ? true : false;
+				}
+				else {
+					console.log(response.message);
+				}
+			},
+			function (error) {
+				console.log(error);
+			}
+		);
+	}
+
+	// Save Thunderdome settings
+	$scope.updateThunderdome = function () {
+		var twitter = $scope.twitter;
+		var quiet_hours = JSON.stringify($scope.quiet_hours);
+		var enabled = $scope.enabled ? 1 : 0;
+		SettingsService.updateThunderdome(twitter, quiet_hours, enabled,
+			function (response) {
+				if (response.status) {
+					console.log("Success");
+				}
+				else {
+					console.log(response.message);
+				}
+			},
+			function (error) {
+				console.log(error);
+			}
+		)
+	}
+
+	// Delete Thunderdome settings
+	$scope.deleteThunderdome = function () {
+		SettingsService.deleteThunderdome(
+			function (response) {
+				if (response.status) {
+					console.log("Success");
+					$scope.twitter = "";
+					$scope.quiet_hours = [];
+					$scope.enabled = true;
+				}
+				else {
+					console.log(response.message);
+				}
+			},
+			function (error) {
+				console.log(error);
+			}
+		);
+	}
+
 	// Check for the user's API key
 	$scope.retrieveKey();	
+	// Get a user's Thunderdome settings
+	$scope.getThunderdome();
 }
 
 function ThunderdomeCtrl($scope, $log, MachineService, ThunderdomeService) {
