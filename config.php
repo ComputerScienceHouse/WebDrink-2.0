a9f8d09a71f98555738a58e9c1de20edc2687f0c
Ben Centra <blcentra@gmail.com>
2014-07-13 12:53:05 -0400

    fixing whitespace in config.php, caused issues in live version

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
- <?php
+<?php
   
- define("DEBUG", false);
- define("API_BASE_URL", "api/index.php?request="); // For no/broken .htaccess mod_rewrite
- //define("API_BASE_URL", "api/"); // What it should be
+define("DEBUG", true);
+define("API_BASE_URL", "api/index.php?request="); // For no/broken .htaccess mod_rewrite
+//define("API_BASE_URL", "api/"); // What it should be
   
-  ?>
+?>
