02f90d279425b0e5813ac8b9b2a038ef3e501896
Ben Centra <bcentra@constantcontact.com>
2015-01-28 12:53:07 -0500

    adding disabled states, drink dropping to touchscreen

--- touchscreen/js/touchscreen.js
+++ touchscreen/js/touchscreen.js
@@ -11,7 +11,9 @@ app.directive('drink', function() {
     restrict: "E",
     transclude: true,
     scope: {
-      drink: "=data"
+      drink: "=data",
+      drop: "=drop",
+      disabled: "=disabled"
     },
     templateUrl: "directives/drink.html"
   };
@@ -33,29 +35,48 @@ app.factory("TouchscreenService", ["$http", function($http) {
         console.error(resp.message);
       }
     };
+  };
+
+  var ajaxSuccess = function(cbPass, cbFail) {
+    return function(resp) {
+      if (resp.status === true) {
+        cbPass(resp.data);
+      } else {
+        if (typeof cbFail === "function") {
+          cbFail(resp.message);
+        } else {
+          console.error(resp.message);
+        }
+      }
+    };
   }
 
-  var error = function(e) {
+  var ajaxError = function(e) {
     console.error(e);
   };
 
   return {
-    getUser: function(ibutton, callback) {
-      $http.get(apiUrl+"?request=users/info&ibutton="+ibutton).success(success(callback)).error(error);
+    getUser: function(ibutton, pass, fail) {
+      $http.get(apiUrl+"?request=users/info&ibutton="+ibutton).success(ajaxSuccess(pass, fail)).error(ajaxError);
     },
-    getStock: function(machine, callback) {
-      $http.get(apiUrl+"?request=machines/stock&machine_id="+machine).success(success(callback)).error(error);
+    getStock: function(machine, pass, fail) {
+      $http.get(apiUrl+"?request=machines/stock&machine_id="+machine).success(ajaxSuccess(pass, fail)).error(ajaxError);
     },
-    getStatus: function(callback) {
-      $http.get(apiUrl+"?request=drops/status").success(success(callback)).error(error);
+    getStatus: function(ibutton, pass, fail) {
+      $http.get(apiUrl+"?request=drops/status&ibutton="+ibutton).success(ajaxSuccess(pass, fail)).error(ajaxError);
     },
-    dropDrink: function(ibutton, machine, slot, callback) {
-      $http.post(apiUrl, {
-        request: "drops/drop",
+    dropDrink: function(ibutton, machine, slot, pass, fail) {
+      var data = {
         ibutton: ibutton,
         machine_id: machine,
         slot_num: slot
-      }).success(success(callback)).error(error);
+      };
+      $http({
+        method: "POST",
+        url: apiUrl+"?request=drops/drop",
+        data: jQuery.param(data),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).success(ajaxSuccess(pass, fail)).error(ajaxError);
     }
   };
 
@@ -64,7 +85,9 @@ app.factory("TouchscreenService", ["$http", function($http) {
 /*
 * TouchscreenController
 */
-app.controller("TouchscreenController", ["$scope", "TouchscreenService", function($scope, TouchscreenService) {
+app.controller("TouchscreenController", ["$scope", "$timeout", "TouchscreenService", function($scope, $timeout, TouchscreenService) {
+
+  var resetTimeout;
 
   var machineId = (function() {
     var search = window.location.search;
@@ -84,13 +107,16 @@ app.controller("TouchscreenController", ["$scope", "TouchscreenService", functio
     $scope.user = {};
     $scope.message = "Touch iButton To Continue";
     $scope.detail = false;
+    $timeout.cancel(resetTimeout);
   };
   $scope.logout = reset;
 
   var authenticate = function(ibutton) {
     $scope.ibutton = ibutton;
+    checkConnection();
     getUserInfo();
     getMachineStock();
+    resetTimeout = $timeout(reset, 10000);
   };
   $scope.login = authenticate;
 
@@ -102,17 +128,62 @@ app.controller("TouchscreenController", ["$scope", "TouchscreenService", functio
   };
 
   var getMachineStock = function() {
-    if (!machineId) return false;
+    if (machineId === false) return false;
     TouchscreenService.getStock(machineId, function(data) {
       $scope.stock = data[machineId];
     });
   };
 
   var checkConnection = function() {
-    TouchscreenService.getStatus(function(data) {
-      $scope.connected = data;
-    });
+    TouchscreenService.getStatus(
+      $scope.ibutton, 
+      function(data) {
+        $scope.connected = data;
+      },
+      function(msg) {
+        console.error(msg);
+        $scope.connected = false;
+      }
+    );
   };
+
+  var dropDrink = function(drink) {
+    if (isDrinkDisabled(drink)) return false;
+    if ($scope.connected === false) return false;
+    var closeModal = function() {
+      $timeout(function() {
+        $("#drop").modal("hide");
+        reset();
+      }, 2000);
+    }
+    $scope.drop_message = "Dropping " + drink.item_name + "..."
+    $timeout.cancel(resetTimeout);
+    $("#drop").modal("show");
+    TouchscreenService.dropDrink(
+      $scope.ibutton, 
+      drink.machine_id, 
+      drink.slot_num, 
+      function(data) {
+        $scope.drop_message = "Drink Dropped!";
+        closeModal();
+      },
+      function(msg) {
+        $scope.drop_message = msg;
+        closeModal();
+      }
+    );
+  };
+  $scope.drop = dropDrink;
+
+  var isDrinkDisabled = function(drink) {
+    if (drink.status !== 'enabled' ||
+        parseInt(drink.available) === 0 ||
+        parseInt(drink.item_price) > parseInt($scope.user.credits)) {
+      return true;
+    }
+    return false;
+  }
+  $scope.disabled = isDrinkDisabled;
 
   reset();
 
